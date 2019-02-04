0e5ec45a85bbfa3a8219e56a62c7e87a23357b70
Kevin Dinh <kevdinh33@gmail.com>
2019-02-04 15:59:59 -0600

    Debugged search function.

--- linkedList/LinkedList.hpp
+++ linkedList/LinkedList.hpp
@@ -38,7 +38,7 @@ bool LinkedList<T>::search(T value) const
 	Node<T>* temp = m_front;
 	bool isFound = false;
 
-	for (size_t i = 0; i < m_size; i++)
+	for (int i = 0; i < size(); i++)
 	{
 		if(temp->getValue() == value)
 		{
